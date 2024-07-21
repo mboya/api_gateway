a709e330d0150ba5b48d9dba8f905e00ede6b02e
Mizu San <mizu@Mboyas-MacBook-Pro.local>
2024-07-21 21:21:51 +0300

    setup the app to run on docker, disabled ssl for production(TBD)

--- config/application.rb
+++ config/application.rb
@@ -28,5 +28,9 @@ module ApiGateway
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    console do
+      ActiveRecord.connection
+    end
   end
 end
