3f355ac5c6fd49eba5daddeaf72d5ac91a040aad
Mizu San <mizu@Mboyas-MacBook-Pro.local>
2024-07-23 15:40:27 +0300

    remove unused routes at the moment from the gateway

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,30 @@
-require_relative "boot"
+require_relative 'boot'
 
-require "rails/all"
+# require "rails/all"
+require 'rails'
+
+# This list is here as documentation only - it's not used
+omitted = %w[
+  active_storage/engine
+  action_mailbox/engine
+  action_text/engine
+]
+
+# Only the frameworks in Rails that do not pollute our routes
+%w[
+  action_cable/engine
+  active_record/railtie
+  action_controller/railtie
+  action_view/railtie
+  action_mailer/railtie
+  active_job/railtie
+  rails/test_unit/railtie
+  sprockets/railtie
+].each do |railtie|
+  require railtie
+rescue LoadError
+   # Ignored
+end
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -10,12 +34,12 @@ module ApiGateway
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.1
-    
+
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w[assets tasks])
-    
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
@@ -23,16 +47,16 @@ module ApiGateway
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
-    
+
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
-    
+
     console do
       ActiveRecord::Base.connection
     end
-    
+
     config.active_record.primary_key = :uuid
   end
 end
